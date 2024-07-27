f7f69ef3ac579f18787d816cf46fcbbde862750f
Fernando Maschio <nando.maschio@icloud.com>
2024-07-27 16:07:31 -0300

    aula praticando a criação de uma calculadora, fiz somente a parte lógica com JS

--- src/script.js
+++ src/script.js
@@ -3,6 +3,39 @@
 
 //ESCREVA SEU CÓDIGO AQUI!!
 
+function Calculadora(numero1, numero2, operador) {
+
+  let resultado
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      let contador
+      resultado = num1 * num2
+      while (contador < num2) {
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  }
+
+  if (resultado == "undefined" || resultado > 10000)
+    resultado = "ERRO"
+  return resultado
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
